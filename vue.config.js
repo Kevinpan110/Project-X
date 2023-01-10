fce7105c0a4bdf3f061556b3c7030a945c9ae827
kevin <408340739@qq.com>
2023-01-10 20:22:47 +0800

    开启gzip，加入bundle分析

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
+//treeshake
 const TerserPlugin = require("terser-webpack-plugin");
 //测速工具
 const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
-const path = require('path');
+//Gzip压缩
+const CompressionPlugin = require("compression-webpack-plugin");
+//webpack-bundle-analyzer
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const path = require("path");
 // 路径处理方法
-function resolve(dir){
+function resolve(dir) {
   return path.join(__dirname, dir);
 }
 module.exports = defineConfig({
@@ -20,36 +25,53 @@ module.exports = defineConfig({
       },
     },
   },
+  chainWebpack: (config) => {
+      config.plugin("compression-webpack-plugin").use(
+        new CompressionPlugin({
+          test: /\.js$|\.html$|\.css$/, // 匹配文件名
+          threshold: 10240, // 对超过10k的数据压缩
+          deleteOriginalAssets: false, // 不删除源文件
+        })
+      );
+      // const imgRule = config.module.rule('images');
+      // imgRule
+      //     .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+      //     .use('image-webpack-loader')
+      //     .loader('image-webpack-loader')
+      //     .options({ bypassOnDebug: true })
+      //     .end();
+  },
   configureWebpack: {
-    module: { 
+    module: {
       noParse: /(^jquery$)|(^lodash$)|(^vue$)|(^pinia$)|(^vue-router$)/,
       rules: [
         {
           test: /\.js$/i,
-          include: resolve('src'),
+          include: resolve("src"),
           exclude: /node_modules/,
-          use: [
-            'babel-loader',
-          ]
+          use: ["babel-loader"],
         },
-      ]
+      ],
     },
     cache: {
       type: "filesystem",
     },
-    plugins: [new SpeedMeasurePlugin()],
+    plugins: [new SpeedMeasurePlugin(), new BundleAnalyzerPlugin()],
+    //开启生产模式
+    mode: "production",
     //ESBuild编译器
     optimization: {
+      usedExports: true,
       minimize: true, // 开启最小化
       minimizer: [
         new TerserPlugin({
           //swc压缩
-          minify:TerserPlugin.swcMinify,
+          minify: TerserPlugin.swcMinify,
           //esbuild压缩
           //minify:TerserPlugin.esbuildMinify
-          terserOptions:{},
-        })
-      ]
+          terserOptions: {},
+        }),
+      ],
     },
   },
 });
